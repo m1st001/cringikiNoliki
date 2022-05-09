caafac578296174ddbda83dae7937a48c5e11d3e
m1sty <m.a.stepanov@outlook.com>
2022-05-09 20:20:52 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.Exit = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(97, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Крестики нолики";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(240, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(37, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(283, 57);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(37, 22);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(197, 85);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(37, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(240, 85);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(37, 22);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(283, 85);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(37, 22);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(197, 113);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(37, 22);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(240, 113);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(37, 22);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(283, 113);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(37, 22);
+            this.textBox9.TabIndex = 9;
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.IndianRed;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Exit.Location = new System.Drawing.Point(379, 174);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 10;
+            this.Exit.Text = "Выход";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Ход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(53, 91);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(37, 13);
+            this.status.TabIndex = 12;
+            this.status.Text = "status";
+            this.status.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(514, 218);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label status;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!CheckSymbols())
+            {
+                MessageBox.Show("Введенный текст не 'х' и не 'о'", "КрИтИчесКая АшибКА", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            status.Text = CheckForWin();
+            if (status.Text.Length > 19)
+            {
+                MessageBox.Show($@"{status.Text}", "Win!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Restart();
+            }
+
+            if (CheckForDraw())
+            {
+                MessageBox.Show($@"Ничья!", "Draw", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Application.Restart();
+            }
+        }
+
+        private string CheckForWin()
+        {
+            if (textBox1.Text.Length > 0) //Проверяем 3 линии из верхнего левого угла.
+            {
+                if (textBox1.Text.Equals(textBox2.Text) && textBox1.Text.Equals(textBox3.Text)) 
+                {
+                    return $@"Игрок '{textBox1.Text}' побеждает.";
+                }
+
+                if (textBox1.Text.Equals(textBox4.Text) && textBox1.Text.Equals(textBox7.Text))
+                {
+                    return $@"Игрок '{textBox1.Text}' побеждает.";
+                }
+
+                if (textBox1.Text.Equals(textBox5.Text) && textBox1.Text.Equals(textBox9.Text))
+                {
+                    return $@"Игрок '{textBox1.Text}' побеждает.";
+                }
+            }
+
+            if (textBox5.Text.Length > 0) //Проверяем 2 линии из через центр.
+            {
+                if (textBox2.Text.Equals(textBox5.Text) && textBox2.Text.Equals(textBox8.Text)) 
+                {
+                    return $@"Игрок '{textBox5.Text}' побеждает.";
+                }
+
+                if (textBox4.Text.Equals(textBox5.Text) && textBox4.Text.Equals(textBox6.Text))
+                {
+                    return $@"Игрок '{textBox5.Text}' побеждает.";
+                }
+            }
+
+            if (textBox3.Text.Length > 0) //Проверяем 3 оставшиеся линии.
+            {
+                if (textBox3.Text.Equals(textBox5.Text) && textBox3.Text.Equals(textBox7.Text)) 
+                {
+                    return $@"Игрок '{textBox3.Text}' побеждает.";
+                }
+
+                if (textBox3.Text.Equals(textBox6.Text) && textBox3.Text.Equals(textBox9.Text))
+                {
+                    return $@"Игрок '{textBox3.Text}' побеждает.";
+                }
+
+                if (textBox7.Text.Equals(textBox8.Text) && textBox7.Text.Equals(textBox9.Text))
+                {
+                    return $@"Игрок '{textBox7.Text}' побеждает.";
+                }
+            }
+            return "Игра продолжается.";
+        }
+
+        private bool CheckForDraw()
+        {
+            int counter = 0;
+            foreach (TextBox tb in this.Controls.OfType<TextBox>())
+            {
+                if (tb.Text.Length > 0)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 9)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckSymbols()
+        {
+            string symbols = "xo";
+            foreach (TextBox tb in this.Controls.OfType<TextBox>())
+            {
+                if (tb.Text.Length == 0)
+                {
+                    continue;
+                }
+
+                if (tb.Text.Length > 1)
+                {
+                    return false;
+                }
+
+                if (!symbols.Contains(tb.Text[0]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
